c139c430ca61f5d2950d6c17a4f996e778c656ff
jinze1994 <baijinze1994@163.com>
2017-01-12 00:01:03 +0800

    fix login & signup mass

--- routers/account.go
+++ routers/account.go
@@ -6,8 +6,8 @@ import (
 )
 
 func init() {
-	beego.Router("/signup", &controllers.AccountController{}, "get:GetLogin")
-	beego.Router("/login", &controllers.AccountController{}, "get:GetSignup")
+	beego.Router("/signup", &controllers.AccountController{}, "get:GetSignup")
+	beego.Router("/login", &controllers.AccountController{}, "get:GetLogin")
 	beego.Router("/account/signup", &controllers.AccountController{}, "post:SignUp")
 	beego.Router("/account/login", &controllers.AccountController{}, "post:Login")
 	beego.Router("/account/logout", &controllers.AccountController{}, "post:Logout")
